356ad16fa92601197300a85934847a9946d3baa6
bamonde <bamonde@bamondePC.(none)>
2011-05-26 17:56:37 -0300

    Aplicação Rails criada.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evento::Application.config.secret_token = '7df4aadd23494a50f30509a168c182230ca258315ba3622a92aaa3b6bba6ed424f988bea63e6708e68dac59436fa1c3e89fecfc7be47e1bf3efd5789d52149c4'
