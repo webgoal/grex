da0e656a0b0405fe1f3c06930a2e41835aebeb29
bamonde <bamonde@bamondePC.(none)>
2011-06-14 18:33:06 -0300

    Removendo os limites do varchar da migration.

--- db/migrate/20110613195138_create_talks.rb
+++ db/migrate/20110613195138_create_talks.rb
@@ -2,13 +2,13 @@ class CreateTalks < ActiveRecord::Migration
   def self.up
     create_table :talks, { :options => "ENGINE=InnoDB", :primary_key => :id } do |t|
       t.column :id, "INT UNSIGNED", :null => false
-      t.string :title, :limit => 60
-      t.string :language, :limit => 30
-      t.string :level, :limit => 40
+      t.string :title
+      t.string :language
+      t.string :level
       t.integer :attendees_limit
-      t.string :type, :limit => 40
-      t.string :track, :limit => 40
-      t.string :room, :limit => 20
+      t.string :type
+      t.string :track
+      t.string :room
       t.boolean :translation
       t.integer :duration, :null => false
       t.datetime :start, :null =>false
