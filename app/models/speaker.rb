94bd026704fa5ff05768f6c3b4722f9e6e3ce94b
Michael Kuhinica <taverneiro@gmail.com>
2011-06-22 20:51:40 -0300

    Adicionados relacionamentos entre palestras e palestrantes

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,2 +1,3 @@
 class Speaker < ActiveRecord::Base
+  has_and_belongs_to_many :talks, :join_table => "talks_speakers"
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,2 +1,7 @@
 class Talk < ActiveRecord::Base
+  has_and_belongs_to_many :speakers, :join_table => "talks_speakers"
+  
+  set_inheritance_column do
+    original_inheritance_column + "_id"
+  end
 end
