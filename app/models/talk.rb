ac4cd9e09a19da987628bfeefb2d8ed1f3656588
Michael Kuhinica <taverneiro@gmail.com>
2011-06-23 01:58:25 -0300

    Listagem dinamica lendo do banco terminada

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,15 @@
 class Talk < ActiveRecord::Base
   has_and_belongs_to_many :speakers, :join_table => "talks_speakers"
   
+  scope :in_order, order(:start)
+  scope :from_track, lambda {|track| where('track like ?', '%'+track+'%')}
+  scope :by_day, lambda {|date| where('date(start) = ?', date)}
+  
+  def self.track_day(date, track)
+    self.by_day(date).from_track(track).in_order
+  end
+  
+  #Sobrescreve o nome da coluna de herança padrão do Rails type para type_id, evita conflitos
   set_inheritance_column do
     original_inheritance_column + "_id"
   end
