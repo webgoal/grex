f0d4d93ba8e71308467dc140c519787c67a89014
Michael Kuhinica <taverneiro@gmail.com>
2011-06-23 12:23:45 -0300

    Adicionado live para eventos de swipe (evita bug ao carregar conteudo por ajax)

--- public/javascripts/evento.js
+++ public/javascripts/evento.js
@@ -9,13 +9,13 @@ jQuery(document).ready(function(){
 });
 
 function bindSwipe(dia) {
-	jQuery(dia+' .trilha').bind('swipeleft', function(e){
+	jQuery(dia+' .trilha').live('swipeleft', function(e){
 		trilha = jQuery(this);
 		if(trilha.next(dia+' .trilha').length){
 			trilha.hide().next(dia+' .trilha').show();
 		}
 	});
-	jQuery(dia+' .trilha').bind('swiperight', function(e){
+	jQuery(dia+' .trilha').live('swiperight', function(e){
 		trilha = jQuery(this);
 		if(trilha.prev(dia+' .trilha').length) {
 			trilha.hide().prev(dia+' .trilha').show();
