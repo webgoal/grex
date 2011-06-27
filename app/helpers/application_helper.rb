a2d9057e7b292492e2c0c501633ea89012602c01
imac <imac@iMac-de-imac.local>
2011-06-27 15:30:17 -0300

    formatando detalhes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def is_talk_selected?(talk_id, selected_list)
     " active " if selected_list.include? talk_id
   end
+  
+  def show_talk_level(level) 
+      ("\<span class='"+ActiveSupport::Multibyte::Chars.new(level.downcase).normalize(:kd).gsub(/[^\x00-\x7F]/,'') +"' \>"+ level +"\</span\>").html_safe unless level.nil?
+  end
 end
--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -1,7 +1,3 @@
 module ScheduleHelper
-  
-  def show_talk_level(level) 
-      ("\<span class='"+ActiveSupport::Multibyte::Chars.new(level.downcase).normalize(:kd).gsub(/[^\x00-\x7F]/,'') +"' \>"+ level +"\</span\>").html_safe unless level.nil?
-  end
 
 end
