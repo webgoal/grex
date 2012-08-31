dc02b8a25535914c73feb61d2418f49112c2e3b2
Rafael Valerio <rafael.v90@gmail.com>
2012-08-31 16:13:47 -0300

    Correção na disposição dos elementos da lista

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class SchedulesController < ApplicationController
   protect_from_forgery
   
@@ -7,22 +8,14 @@ class SchedulesController < ApplicationController
     @my_user = current_user
 
     params[:date] ||= "2012-09-05" #Time.new.strftime("%Y-%m-%d")
-
-    @tracks2 = {
-      :geral => Talk.track_day(params[:date], "geral"),
-      :engenharia => Talk.track_day(params[:date], "engenharia"),
-      :relatos => Talk.track_day(params[:date], "relatos"),
-      :gestao => Talk.track_day(params[:date], "gest"),
-      :principal => Talk.track_day(params[:date], "principal")
-    }
     
     @tracks = {
-      :desenvolvimento => Talk.track_day(params[:date], "desenvolvimento"),
-      :analise => Talk.track_day(params[:date], "lise"),
-      :gestao => Talk.track_day(params[:date], "gest"),
-      :inovacao => Talk.track_day(params[:date], "inova"),
-      :relatos => Talk.track_day(params[:date], "relatos"),
-      :convidados => Talk.track_day(params[:date], "convidado")
+      :convidados => {:name => "Convidados", :talks => Talk.track_day(params[:date], "convidado")},
+      :desenvolvimento => {:name => "Desenvolvimento e Teste", :talks => Talk.track_day(params[:date], "desenvolvimento")},
+      :analise => {:name => "Análise e Planejamento", :talks => Talk.track_day(params[:date], "lise")},
+      :gestao => {:name => "Gestão e Cultura", :talks => Talk.track_day(params[:date], "gest")},
+      :inovacao => {:name => "Inovação e Empreendedorismo", :talks => Talk.track_day(params[:date], "inova")},
+      :relatos => {:name => "Relatos de Experiência", :talks => Talk.track_day(params[:date], "relatos")},
     }
     
     @user_tracks = current_user.talk_ids params[:date]
